6158f704cf4c41bb979b5c5cd003f6f1d859e290
tomaskurten <tomas.kurten@bue.edu.ar>
2024-04-17 21:19:43 -0300

    cambios html y scripts

---
+++ script.js
@@ -0,0 +1,46 @@
+console.log("Hola desde script")
+var num3=5
+var num2=10
+console.log(num3+num2)
+console.clear()
+var IVA= 21;
+IVA= 10.5;
+console.log(IVA);
+const PI=3.14
+// // PI= 1; NO SE PUEDE POR SER CONST
+console.log(PI);
+// console.log("Hola Mundo!")
+// console.log("El resultado de la suma es: ",3+5)
+console.clear()
+var variable=5
+console.log(variable)
+console.log(typeof variable)
+variable="Perro!565756"
+console.log(variable)
+console.log(typeof variable)
+variable=true
+console.log(variable)
+console.log(typeof variable)
+console.clear()
+var x=Infinity
+console.log(Number.isFinite(x))
+
+
+console.clear()
+var y="5"
+console.log( y +1)
+
+console.log( parseInt(y) +1)
+console.log( parseFloat(y) +0.1)
+
+
+console.clear()
+console.log(5+6)
+console.log(5-6)
+console.log(5*6)
+console.log(5/6)
+console.log(3**9)
+var z=5
+console.log(z+1)
+q="5"+"6"+"Tomas"
+console.log(parseInt(q))
