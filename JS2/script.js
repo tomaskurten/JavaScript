6322f0ee62dcb652b93e0e10d8bcdb197ba9f40d
tomaskurten <tomas.kurten@bue.edu.ar>
2024-04-22 21:28:37 -0300

    agregue JS2

---
+++ JS2/script.js
@@ -0,0 +1,101 @@
+// a=1
+// b=2
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a>b)
+// console.log(a<b)
+
+// logica
+// console.log("and",a>2 && b==2)
+// console.log("and",a<2 && b==2)
+// console.log("or",a>2 || b==2)
+// console.log("or",a>2 || b==4)
+// console.log("negacion",!(a==b))
+
+
+//prefijo posfijo
+
+// i=1
+// a=i++
+// console.log(a)
+// console.log(i)
+
+// i=1
+// a=++i
+// console.log(a)
+// console.log(i)
+
+
+// x=1
+// y=3
+// x+=y
+// console.log(x)
+
+
+// condicionales
+// nota=2
+
+// if (nota>=4){
+
+//     if(nota>=6){
+//         console.log("Promocionaste!")
+//     }
+//     else{
+//         console.log("Aprobaste!")
+//     }
+    
+// }
+// else{
+//     if(nota==3){
+//         console.log("D, Tenes recuperatorio especial")
+//     }
+//     else{
+//         console.log("Desaprobaste :(")
+//     }
+    
+// }
+// console.log("Fin")
+
+
+// opcion=parseInt(prompt("Ingrese opcion 1 2 o 3"))
+// switch(opcion){
+//     case 1:
+//         console.log("opcion 1 elegida")
+//         break
+//     case 2:
+//         console.log("opcion 2 elegida")
+//         break
+//     case 3:
+//         console.log("opcion 3 elegida")
+//         break
+//     default:
+//         console.log("Opcion inexistente!")
+// }
+
+
+// var altura = 0;
+// var edad = 0;
+// altura = parseFloat(prompt("Ingrese la altura"));
+// edad = parseInt(prompt("Ingrese la edad"));
+// if (altura > 1.30 && edad > 14) {
+//  console.log("Cumple con los requisitos")
+// } else{
+//  console.log("No cumple con los requisitos")}
+
+
+// BUCLES
+// WHILE
+
+// i=0 // Inicialización de la variable contador
+// // Condición: Mientras la variable contador sea menor de 5
+// while (i < 5) {
+//  console.log("Valor de i:", i);
+//  i = i + 1; // Incrementamos el valor de i
+// }
+// console.log("sali del while")
+
+// FOR
+for (var i=0; i<5; i--) {
+    console.log("Valor de i:", i);
+   }
+   
